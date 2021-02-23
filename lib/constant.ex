d7958e98871b1c46f92e2ccef39d174b88963581
Vasu Adari <_@vasuadari.com>
2021-02-23 22:14:11 +0530

    Dynamically add boolean function for each constant key

--- lib/constant.ex
+++ lib/constant.ex
@@ -1,28 +1,30 @@
 defmodule Constant do
   defmacro defconst(list, opts \\ []) do
-    quote bind_quoted: [list: list, opts: opts] do
+    quote bind_quoted: [list: list, opts: opts], location: :keep do
       import String, only: [to_atom: 1]
 
       Enum.each(list, fn
-        {key, value} when is_atom(key) and is_binary(value) ->
-          if opts[:atomize] do
-            escaped_value = Macro.escape(value)
-
-            def unquote(key)(), do: unquote(escaped_value)
-
-            escaped_atom_value = Macro.escape(to_atom(value))
-
-            def unquote(:"#{key}_atom")(), do: unquote(escaped_atom_value)
-          else
-            escaped_value = Macro.escape(value)
-
-            def unquote(key)(), do: unquote(escaped_value)
-          end
+        {key, value} when is_nil(value) ->
+          raise "Value cannot be nil"
 
         {key, value} when is_atom(key) ->
           escaped_value = Macro.escape(value)
 
           def unquote(key)(), do: unquote(escaped_value)
+
+          if opts[:atomize] && is_binary(value) do
+            escaped_atom_value = Macro.escape(to_atom(value))
+
+            def unquote(:"#{key}_atom")(), do: unquote(escaped_atom_value)
+
+            def unquote(:"#{key}?")(value) do
+              value in [unquote(escaped_value), unquote(escaped_atom_value)]
+            end
+          else
+            def unquote(:"#{key}?")(value) do
+              value == unquote(escaped_value)
+            end
+          end
 
         _ ->
           raise "Key has to be an atom"
--- test/constant_test.exs
+++ test/constant_test.exs
@@ -1,46 +1,66 @@
 defmodule ConstantTest do
   use ExUnit.Case, async: true
 
-  defmodule TestConstant do
+  defmodule IntegerTest do
     import Constant
 
     defconst(a: 1)
+  end
+
+  defmodule StringTest do
+    import Constant
 
     defconst([b: "c"], atomize: true)
+  end
+
+  defmodule MapTest do
+    import Constant
 
     defconst(c: %{a: 2})
+  end
+
+  defmodule TupleTest do
+    import Constant
 
     defconst(d: {0, 0})
+  end
+
+  defmodule ListTest do
+    import Constant
 
     defconst(e: [1, 2, 3])
+  end
+
+  defmodule KeywordTest do
+    import Constant
 
     defconst(f: [a: 1, b: 2])
   end
 
   test "returns integer for a constant" do
-    assert TestConstant.a() == 1
+    assert IntegerTest.a() == 1
   end
 
   test "returns atom & string constant when atomize_values option is true" do
-    assert TestConstant.b() == "c"
+    assert StringTest.b() == "c"
 
-    assert TestConstant.b_atom() == :c
+    assert StringTest.b_atom() == :c
   end
 
   test "returns map for a constant" do
-    assert TestConstant.c() == %{a: 2}
+    assert MapTest.c() == %{a: 2}
   end
 
   test "returns tuple for a constant" do
-    assert TestConstant.d() == {0, 0}
+    assert TupleTest.d() == {0, 0}
   end
 
   test "returns list for a constant" do
-    assert TestConstant.e() == [1, 2, 3]
+    assert ListTest.e() == [1, 2, 3]
   end
 
   test "returns keyword list for a constant" do
-    assert TestConstant.f() == [a: 1, b: 2]
+    assert KeywordTest.f() == [a: 1, b: 2]
   end
 
   test "raises exception when key is not an atom" do
@@ -50,6 +70,34 @@ defmodule ConstantTest do
 
         defconst([{"g", 1}])
       end
+    end
+  end
+
+  test "raises exception when value cannot be nil" do
+    assert_raise RuntimeError, "Value cannot be nil", fn ->
+      defmodule NilTest do
+        import Constant
+
+        defconst(h: nil)
+      end
+    end
+  end
+
+  describe "{key}?/1" do
+    test "returns true when a is 1" do
+      assert IntegerTest.a?(1)
+    end
+
+    test "returns false when a is 2" do
+      refute IntegerTest.a?(2)
+    end
+
+    test "returns true when b is string c" do
+      assert StringTest.b?("c")
+    end
+
+    test "returns true when b is atom c" do
+      assert StringTest.b?(:c)
     end
   end
 end
